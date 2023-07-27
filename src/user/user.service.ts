b08678180d50b3c1b3f28f3353a4df1e77055468
lgklsv <kolosov20011@gmail.com>
2023-07-28 01:10:56 +0300

    feat: make db service available globally

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
 export class UserService {
+  constructor(private db: DatabaseService) {}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }

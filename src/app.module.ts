bb19d9456cd24da067d255367e075f12e6cfa155
lgklsv <kolosov20011@gmail.com>
2023-07-28 00:16:01 +0300

    feat: add config module to enable .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule],
+  imports: [ConfigModule.forRoot(), DatabaseModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
   exports: [],

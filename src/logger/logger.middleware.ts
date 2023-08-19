870098db81aaa51933d67d765f3245ea888bbd4a
lgklsv <kolosov20011@gmail.com>
2023-08-19 21:26:01 +0300

    feat: log more data in http logs middleware

--- src/logger/logger.middleware.ts
+++ src/logger/logger.middleware.ts
@@ -7,17 +7,19 @@ export class LoggerMiddleware implements NestMiddleware {
   private logger = new HomeLibraryServiceLogger('HTTP');
 
   use(request: Request, response: Response, next: NextFunction): void {
-    const { ip, method, originalUrl } = request;
+    const { ip, method, originalUrl, query, body } = request;
     const userAgent = request.get('user-agent') || '';
 
     response.on('finish', () => {
       const { statusCode } = response;
-      const contentLength = response.get('content-length');
 
-      this.logger.log(
-        `${method} ${originalUrl} ${statusCode} ${contentLength} - ${userAgent} ${ip}`,
-        'HTTP',
-      );
+      const message = `${method} ${originalUrl} ${statusCode} query parameters: ${JSON.stringify(
+        query,
+      )}, body: ${JSON.stringify(body)} - ${userAgent} ${ip}`;
+
+      if (statusCode < 400) {
+        this.logger.log(message, 'HTTP');
+      }
     });
 
     next();
